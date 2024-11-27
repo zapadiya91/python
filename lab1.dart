9c48761336d0c77ee638dd2e30f9c4596ac4afa7
Zapadiya Haresh <142992715+zapadiya91@users.noreply.github.com>
2024-11-27 10:58:59 +0530

    Add files via upload

---
+++ lab1.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+void main() {
+  // // q-1
+  // stdout.write('enter your name: ');
+  // String a = stdin.readLineSync()!;
+  // print(a);
+
+  // // q-2
+  // stdout.write('enter your number 1: ');
+  // int num1 = int.parse(stdin.readLineSync()!);
+  // stdout.write('enter your number 2: ');
+  // int num2 = int.parse(stdin.readLineSync()!);
+  // print('sum of both numbers are ${num1+num2}');
+
+  // // q-3
+  // stdout.write('enter your temperature in fahrenheit : ');
+  // double f = double.parse(stdin.readLineSync()!);
+  // print('temperature in celcius : ${(f-32)*(5/9)}');
+
+  // // q-4
+  //
+  // stdout.write('enter your mark of maths : ');
+  // int m1 = int.parse(stdin.readLineSync()!);
+  // stdout.write('enter your mark of english : ');
+  // int m2 = int.parse(stdin.readLineSync()!);
+  // stdout.write('enter your mark of ds : ');
+  // int m3 = int.parse(stdin.readLineSync()!);
+  // stdout.write('enter your mark of dbms : ');
+  // int m4 = int.parse(stdin.readLineSync()!);
+  // stdout.write('enter your mark of php : ');
+  // int m5 = int.parse(stdin.readLineSync()!);
+  // double percentage = (m1+m2+m3+m4+m5)/500*100;
+  // print('your percentage is $percentage');
+
+  // // q-5
+  //
+  // stdout.write('enter your distance in meter : ');
+  // double meter = double.parse(stdin.readLineSync()!);
+  // print('distance in feet : ${meter* 3.28084}');
+
+  // q-6
+
+  // stdout.write('enter your weight in pounds : ');
+  // double pounds = double.parse(stdin.readLineSync()!);
+  // stdout.write('enter your height in inches : ');
+  // double inches = double.parse(stdin.readLineSync()!);
+  // double kg = pounds*0.45359237;
+  // double meters=inches*0.0254;
+  // print('your weight in kg $kg');
+  // print('your weight in meter $meters');
+  // double BMI = kg/(meters*meters);
+  // print('your BMI is $BMI');
+
+  
+
+}
